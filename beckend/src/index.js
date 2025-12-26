d48588501728787277a6be3890e5f86a089bd64b
Mamta <mamtakri230605@gmail.com>
2025-12-27 01:50:05 +0530

    another commit

--- beckend/src/index.js
+++ beckend/src/index.js
@@ -17,17 +17,27 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
+// ⚠️ prod me localhost mat rakho (optional improvement)
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production" ? true : "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/messages", messageRoutes);
 
+// Frontend serve (ONLY in production)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
