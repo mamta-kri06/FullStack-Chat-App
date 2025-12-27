19e2b8231a9bfb5ace58e8f9d74505384e6cf5ea
Mamta <mamtakri230605@gmail.com>
2025-12-27 12:53:01 +0530

    corrected url

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 // ✅ CORRECT BASE URL
 const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "http://localhost:5001"
+    ? "http://localhost:3000"
     : "https://fullstack-chat-app-1-8i1e.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
